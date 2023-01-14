a7f665a9d1e2b3fbf2eac44bd504d1d44aac3d4c
Federico Freiberger <frei.federico@gmail.com>
2023-01-14 11:00:18 -0300

    add key property

--- components/SpeakersList.js
+++ components/SpeakersList.js
@@ -1,15 +1,19 @@
-import Speaker from "./Speaker";
+import Speaker from "./Speaker"
 
 function SpeakersList({ data, showSessions }) {
 	return (
-		<div className="container speakers-list">
-			<div className="row">
+		<div className='container speakers-list'>
+			<div className='row'>
 				{data.map((speaker) => (
-					<Speaker speaker={speaker} showSessions={showSessions} />
+					<Speaker
+						speaker={speaker}
+						showSessions={showSessions}
+						key={speaker.id}
+					/>
 				))}
 			</div>
 		</div>
-	);
+	)
 }
 
-export default SpeakersList;
+export default SpeakersList
